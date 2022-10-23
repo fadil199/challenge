1e700e5b9abf81f937230189a09455db97b0439b
fadil666 <achmadfadilla197@gmail.com>
2022-10-23 15:38:48 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const user = require('./user');
 const histori  = require('./histori');
 const biodata = require('./biodata');
 
+router.get('/', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: 'hello world'
+    })
+});
+
 router.use('/user', user);
 router.use('/histori', histori);
 router.use('/biodata', biodata);
